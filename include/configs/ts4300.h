e728a986dbf8cac4276ec4a3d256332dda08d2a3
Lionel D. Hummel <lionel@embeddedTS.com>
2025-07-09 17:50:41 +0000

    include: configs: ts4300: Add pxefile_addr_r
    Needed for PXE boot.

--- include/configs/ts4300.h
+++ include/configs/ts4300.h
@@ -61,6 +61,7 @@
 	AHAB_ENV \
 	"prepare_mcore=setenv mcore_clk clk-imx93.mcore_booted;\0" \
 	"scriptaddr=0x83500000\0" \
+	"pxefile_addr_r=0x83200000\0" \
 	"kernel_addr_r=" __stringify(CONFIG_SYS_LOAD_ADDR) "\0" \
 	"image=Image\0" \
 	"splashimage=0x90000000\0" \
