fbc43e42c3302ca1793cda9a949a808272bf4c02
Lionel D. Hummel <lionel@embeddedTS.com>
2025-07-09 01:37:38 +0000

    board: technologic: common-baseboards: Refactor for standard boot
    The function `construct_baseboard_overlay_str()`, factored out of
    `extension_board_scan()`, keeps extensions working while also
    supporting standard boot.

--- board/technologic/ts-common-baseboards/ts_extension_boards.c
+++ board/technologic/ts-common-baseboards/ts_extension_boards.c
@@ -7,25 +7,59 @@
 #include <extension_board.h>
 #include <env.h>
 
-int extension_board_scan(struct list_head *extension_list)
+/*
+ * construct_baseboard_overlay_str
+ *
+ * This function returns `true` if the environment has the variables
+ * needed to construct the name of the dtbo, `false` if it was not.
+ *
+ * Call do_bbdetect() before calling this function. Doing so ensures
+ * the needed environment variables are populated.
+ */
+bool construct_baseboard_overlay_str(char *string_buffer, int buffer_len, char *prefix)
 {
-	struct extension *extension = NULL;
 	uint8_t baseboard_id;
 	char *board;
 	char *soc_type;
+
+	baseboard_id = env_get_hex("baseboard_id", 0);
+	if (!baseboard_id)
+		return false;
+	board = from_env("board");
+	if (!board)
+		return false;
+	soc_type = from_env("soc_type");
+	if (!soc_type)
+		return false;
+
+	if (!prefix)
+		prefix = "";
+	snprintf(string_buffer, buffer_len,
+		 "%s%s-%s-%02x.dtbo", prefix, soc_type, board, baseboard_id);
+	return true;
+}
+
+/*
+ * extension_board_scan
+ *
+ * This function returns "the number of extension boards found", i.e.,
+ * 1 if a recognized/valid extension board was added to the list of
+ * extensions, 0 if not.
+ *
+ * Call do_bbdetect() before calling this function. Doing so ensures
+ * the needed environment variables are populated.
+ */
+int extension_board_scan(struct list_head *extension_list)
+{
+	struct extension *extension = NULL;
 	uint8_t baseboard_rev;
+	uint8_t baseboard_id;
 	bool valid = false;
 
 	baseboard_id = env_get_hex("baseboard_id", 0);
 	if (!baseboard_id)
 		return 0;
 	baseboard_rev = env_get_hex("baseboard_rev", 0);
-	board = from_env("board");
-	if (!board)
-		return 0;
-	soc_type = from_env("soc_type");
-	if (!soc_type)
-		return 0;
 
 	switch (baseboard_id) {
 	case 0x16:
@@ -48,8 +82,8 @@ int extension_board_scan(struct list_head *extension_list)
 
 	snprintf(extension->owner, sizeof(extension->owner),
 		 "embeddedTS");
-	snprintf(extension->overlay, sizeof(extension->overlay),
-		 "%s-%s-%02x.dtbo", soc_type, board, baseboard_id);
+	construct_baseboard_overlay_str(extension->overlay,
+					sizeof(extension->overlay), (char *)0);
 	snprintf(extension->version, sizeof(extension->version),
 		 "%d", baseboard_rev);
 	switch (baseboard_id) {
